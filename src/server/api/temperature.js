3451065bb6239bd815b32bdea941be0703f6cba0
s4kr4 <s4kr4m4@gmail.com>
2018-09-22 15:40:46 +0900

    Update message

--- src/server/api/temperature.js
+++ src/server/api/temperature.js
@@ -16,7 +16,9 @@ router.get('/', async (ctx, next) => {
   const data = file.readFile('/home/pi/python/record.csv')
   const latest = data[data.length - 1]
   const temperature = latest[1]
-  const message = `現在の室温は${temperature}度です`
+  const date = new Date()
+  const hour = date.getHours()
+  const message = `${hour}時です。現在の室温は${temperature}度です。`
 
   googlehome.notify(message, res => {
     console.log(res)
